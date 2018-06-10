ddaaa830e2cd58dc39ef08ffd8f6ddda2e9b14a1
Dushan Aluth Patabendige <apdushan@gmail.com>
2018-06-11 01:49:53 +0800

    Added markers on google map

--- app/src/main/java/com/epgeotrack/app/ep_geo_tracking/WorkPlacesActivity.java
+++ app/src/main/java/com/epgeotrack/app/ep_geo_tracking/WorkPlacesActivity.java
@@ -1,14 +1,23 @@
 package com.epgeotrack.app.ep_geo_tracking;
 
-import android.support.v4.app.FragmentActivity;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.ui.IconGenerator;
+
+import java.util.HashMap;
 
 public class WorkPlacesActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -37,10 +46,36 @@ public class WorkPlacesActivity extends FragmentActivity implements OnMapReadyCa
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+
+        HashMap<LatLng, String> locations = new HashMap<>();
+        locations.put(new LatLng(6.9336686, 79.850047), "Colombo Fort");
+        locations.put(new LatLng(6.9281946, 79.8446079), "Shangri-La Hotel, Colombo");
+        locations.put(new LatLng(6.920566, 79.856158), "Hilton Residencies, Colombo");
+        locations.put(new LatLng(6.9177047, 79.8630878), "Union Chemists, Colombo");
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        for (LatLng latLng : locations.keySet()) {
+            mMap.addMarker(generateMarker(latLng, locations.get(latLng)));
+            builder.include(latLng);
+        }
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(builder.build(), 150);
+        mMap.moveCamera(cameraUpdate);
+        googleMap.animateCamera(cameraUpdate);
+
+    }
+
+    private MarkerOptions generateMarker(LatLng latLng, String title) {
+        TextView text = new TextView(getApplicationContext());
+        text.setText(title);
+        text.setTextColor(Color.parseColor("#ffffff"));
+        text.setPadding(5, 5, 5, 5);
+        IconGenerator generator = new IconGenerator(getApplicationContext());
+        generator.setBackground(getApplicationContext().getDrawable(R.drawable.amu_bubble_mask));
+        generator.setContentView(text);
+        Bitmap icon = generator.makeIcon();
+
+        return new MarkerOptions().position(latLng).icon(BitmapDescriptorFactory.fromBitmap(icon));
     }
 }
