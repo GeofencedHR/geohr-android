a503a51a806fc0e066e03f71bb1362bca33d5888
Dushan Aluth Patabendige <apdushan@gmail.com>
2018-06-24 17:27:05 +0800

    Added work places map to main activity

--- app/src/main/java/com/epgeotrack/app/ep_geo_tracking/MainActivity.java
+++ app/src/main/java/com/epgeotrack/app/ep_geo_tracking/MainActivity.java
@@ -1,6 +1,8 @@
 package com.epgeotrack.app.ep_geo_tracking;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,9 +12,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.ui.IconGenerator;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +44,11 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -83,5 +106,40 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+
+        HashMap<LatLng, String> locations = new HashMap<>();
+        locations.put(new LatLng(6.9336686, 79.850047), "Colombo Fort");
+        locations.put(new LatLng(6.9281946, 79.8446079), "Shangri-La Hotel, Colombo");
+        locations.put(new LatLng(6.920566, 79.856158), "Hilton Residencies, Colombo");
+        locations.put(new LatLng(6.9177047, 79.8630878), "Union Chemists, Colombo");
+
+        // Add a marker in Sydney and move the camera
+        for (LatLng latLng : locations.keySet()) {
+            mMap.addMarker(generateMarker(latLng, locations.get(latLng)));
+            builder.include(latLng);
+        }
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(builder.build(), 150);
+        mMap.moveCamera(cameraUpdate);
+        googleMap.animateCamera(cameraUpdate);
+    }
+
+    private MarkerOptions generateMarker(LatLng latLng, String title) {
+        TextView text = new TextView(getApplicationContext());
+        text.setText(title);
+        text.setTextColor(Color.parseColor("#ffffff"));
+        text.setPadding(5, 5, 5, 5);
+        IconGenerator generator = new IconGenerator(getApplicationContext());
+        generator.setBackground(getApplicationContext().getDrawable(R.drawable.amu_bubble_mask));
+        generator.setContentView(text);
+        Bitmap icon = generator.makeIcon();
+
+        return new MarkerOptions().position(latLng).icon(BitmapDescriptorFactory.fromBitmap(icon));
     }
 }
